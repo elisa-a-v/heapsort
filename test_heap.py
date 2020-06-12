1cfa532c5d68370d58115a9cfac211987ba127c4
Elisa <elisa.anguita@gmail.com>
2020-06-11 20:24:00 -0400

    changed heap to heapqueue

--- test_heap.py
+++ test_heap.py
@@ -4,21 +4,21 @@ from Heap import Heap
 
 class TestHeap(unittest.TestCase):
     def setUp(self) -> None:
-        self.heap = Heap([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
+        self.heapq = Heap([[4, "hola"], [1, "chao"], [3, "qué tal"], [2, "ACAB"], [16, "Todd"], [9, "???"], [10, ":B"]])
 
     def test_HeapSize(self):
-        self.assertEqual(self.heap.size, 10)
+        self.assertEqual(self.heapq.size, 7)
 
     def test_BuildMaxHeap(self):
-        self.heap.buildmaxheap()
-        for i in range(0, self.heap.size, 1):
-            if 2*i + 1 < self.heap.size:
-                self.assertGreaterEqual(self.heap.array[i], self.heap.get_left(i))
-            if 2*i + 2 < self.heap.size:
-                self.assertGreaterEqual(self.heap.array[i], self.heap.get_right(i))
+        self.heapq.buildmaxheap()
+        for i in range(0, self.heapq.size, 1):
+            if 2*i + 1 < self.heapq.size:
+                self.assertGreaterEqual(self.heapq.array[i], self.heapq.get_left(i))
+            if 2*i + 2 < self.heapq.size:
+                self.assertGreaterEqual(self.heapq.array[i], self.heapq.get_right(i))
 
     def test_heapsort(self):
-        self.heap.heapsort()
+        self.heapq.heapsort()
         arr1 = [18, 123, 1, 21, -3, 7]
         arr2 = [-18, 13, 1, 21, -3, 7]
         heap1 = Heap(arr1)
@@ -27,9 +27,9 @@ class TestHeap(unittest.TestCase):
         heap2.heapsort()
         self.assertEqual(heap1.array, sorted(arr1))
         self.assertEqual(heap2.array, sorted(arr2))
-        lista = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+        lista = [[4, "hola"], [1, "chao"], [3, "qué tal"], [2, "ACAB"], [16, "Todd"], [9, "???"], [10, ":B"]]
         lista.sort()
-        self.assertEqual(self.heap.array, lista)
+        self.assertEqual(self.heapq.array, lista)
 
 
 if __name__ == '__main__':
